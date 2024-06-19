0aa509fdffe23270fc14dcd0b6d5312730a299ae
Beqa <tvildiani2001@gmail.com>
2024-06-19 19:07:16 +0200

    mergerd

--- src/player/utils.c
+++ src/player/utils.c
@@ -6,7 +6,7 @@
 /*   By: btvildia <btvildia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 11:57:44 by escura            #+#    #+#             */
-/*   Updated: 2024/06/19 19:04:22 by btvildia         ###   ########.fr       */
+/*   Updated: 2024/06/19 19:06:22 by btvildia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,11 +30,6 @@ float	view_lane_distance(float x1, float y1)
 	float angle = player()->angle;
 	float angle2 = atan2f(y1 - y2, x1 - x2);
 	float diff = angle - angle2;
-
-	if (diff < -PI)
-		diff += 2 * PI;
-	if (diff > PI)
-		diff -= 2 * PI;
 
 	return (distance(x1, y1) * cosf(diff));
 }
