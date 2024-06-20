7518f75b5cedf12f2ead3f49a8745b6022e1e688
Beqa <tvildiani2001@gmail.com>
2024-06-20 11:51:30 +0200

    fixed

--- src/engine/start.c
+++ src/engine/start.c
@@ -6,7 +6,7 @@
 /*   By: btvildia <btvildia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 13:16:13 by escura            #+#    #+#             */
-/*   Updated: 2024/06/19 20:36:44 by btvildia         ###   ########.fr       */
+/*   Updated: 2024/06/20 11:51:02 by btvildia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,65 +28,47 @@ void	draw_cube(int x, int y, int size, int col)
 	}
 }
 
-void draw_wall(int x, int y){
+void	draw_h_line(float height, int color)
+{
+	const t_cube	*c = cube();
+	int				i;
 
-	const t_cube *c = cube();
-	const float dist = distance(x, y);
-	const float height = 80000 / dist;
-
-	int i = 0;
-	int y2 = height;
-
-	printf("dist: %f\n", dist);
-	int blue_intensity = 255 - (int)dist; // decrease blue intensity with distance
-	if(blue_intensity < 0) blue_intensity = 0; // ensure it doesn't go below 0
-	int color = blue_intensity; // blue color
-
-	while(y2 > 0){
-		mlx_pixel_put(c->mlx, c->win, c->x, (y2 + HEIGHT / 6) , color);
-		y2--;
+	i = 0;
+	while (i < height / 2)
+	{
+		mlx_pixel_put(c->mlx, c->win, c->x, HEIGHT / 2 - i, color);
+		mlx_pixel_put(c->mlx, c->win, c->x, HEIGHT / 2 + i, color);
+		i++;
 	}
 }
 
-void draw_line()
+float	get_ray_angle(int x, int y)
 {
-	const t_cube	*c = cube();
-	int				x_a;
-	int				y_a;
+	const t_player	*p = player();
+	float			angle;
+	float			ca;
 
-	x_a = x + CENTER_WIDTH;
-	y_a = y + CENTER_HEIGHT;
-	mlx_pixel_put(c->mlx, c->win, x_a, y_a, color);
+	ca = atan2f(y - p->y_px, x - p->x_px);
+	angle = p->angle - ca;
+	return (angle);
 }
 
 void	draw_wall(int x, int y)
 {
 	const t_cube	*c = cube();
-	const float		dist = view_lane_distance(x, y);
-	int				i;
-	int				color;
-	int				size;
-	int				height;
+	const t_player	*p = player();
+	float			dist;
+	float			line_height;
+	int				blue_intensity;
 
-	height = HEIGHT / dist * BLOCK_SIZE;
-	i = 0;
-	color = 0x004338c3;
-	while (i < height)
-	{
-		size = WIDTH / cube()->map->width * BLOCK_SIZE;
-		put_line_3d(x, y + i, color);
-		i++;
-	}
+	dist = distance(x, y);
+	blue_intensity = 255 - (dist * 255 / 1000);
+	if (blue_intensity < 0)
+		blue_intensity = 0;
+	line_height = (BLOCK_SIZE / dist) * (WIDTH / 2);
+	draw_h_line(line_height, blue_intensity);
 }
 
-float	shifting_angle(float angle)
-{
-	if (angle < 0)
-		angle += 2 * PI;
-	if (angle > 2 * PI)
-		angle -= 2 * PI;
-	return (angle);
-}
 void	draw_line(void)
 {
 	const int		len = 10000;
@@ -108,40 +90,36 @@ void	draw_line(void)
 	y = p->y_px;
 	p->x_dir = cos(p->angle) * 5;
 	p->y_dir = sin(p->angle) * 5;
-
-    int sx = (p->x_dir > 0) ? 1 : -1;
-    int sy = (p->y_dir > 0) ? 1 : -1;
-    int dx = fabsf(p->x_dir * len);
-    int dy = fabsf(p->y_dir * len);
-    int err = dx - dy;
-
-    int i = 0;
-	int j = 0;
-    while (i < WIDTH)
-    {
-		if(is_touching(x / BLOCK_SIZE, y / BLOCK_SIZE, WALL)){
+	sx = (p->x_dir > 0) ? 1 : -1;
+	sy = (p->y_dir > 0) ? 1 : -1;
+	dx = fabsf(p->x_dir * len);
+	dy = fabsf(p->y_dir * len);
+	err = dx - dy;
+	i = 0;
+	j = 0;
+	while (i < WIDTH)
+	{
+		if (is_touching(x / BLOCK_SIZE, y / BLOCK_SIZE, WALL))
+		{
 			// draw_cube(x, y, 3, 0x00FFF000);
 			draw_wall(x, y);
 			j++;
 			break ;
 		}
-
-        // mlx_pixel_put(c->mlx, c->win, x, y, 0x00FF0000);
-
-        int e2 = 2 * err;
-        if (e2 > -dy)
-        {
-            err -= dy;
-            x += sx;
-        }
-        if (e2 < dx)
-        {
-            err += dx;
-            y += sy;
-        }
-
-        i++;
-    }
+		// mlx_pixel_put(c->mlx, c->win, x, y, 0x00FF0000);
+		e2 = 2 * err;
+		if (e2 > -dy)
+		{
+			err -= dy;
+			x += sx;
+		}
+		if (e2 < dx)
+		{
+			err += dx;
+			y += sy;
+		}
+		i++;
+	}
 }
 
 void	draw_player(void)
@@ -152,12 +130,14 @@ void	draw_player(void)
 	draw_cube(p->x_px, p->y_px, 5, 0x00FF0000);
 }
 
-void draw_middle_line()
+void	draw_middle_line(void)
 {
-	const t_cube *c = cube();
+	const t_cube	*c = cube();
+	int				i;
 
-	int i = 0;
-	while(i < WIDTH){
+	i = 0;
+	while (i < WIDTH)
+	{
 		mlx_pixel_put(c->mlx, c->win, i, HEIGHT / 2, 0x0000FF00);
 		i++;
 	}
@@ -179,15 +159,14 @@ int	render(t_cube *c)
 		j = 0;
 		while (j < c->map->width)
 		{
-			if (c->map->map[i][j] == '1')
-				draw_image(wall, j * BLOCK_SIZE, i * BLOCK_SIZE);
+			// if (c->map->map[i][j] == '1')
+			// 	draw_image(wall, j * BLOCK_SIZE, i * BLOCK_SIZE);
 			j++;
 		}
 		i++;
 	}
 	render_player();
 	destroy_image(wall);
-	
 	draw_middle_line();
 	return (0);
 }
